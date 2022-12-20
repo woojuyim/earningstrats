ef95d8f6023cde017321ac7c7f96aa0d9f9ae909
Wooju Yim <woojuyim@usc.edu>
2022-12-20 12:26:55 -0800

    Added standard deviation for 0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='earningstrats',
-    version = "0.1.5",
+    version = "0.1.6",
     description ='Use Stock Earnings Data to formulate strategies',
     long_description = long_description,
     long_description_content_type='text/markdown',
